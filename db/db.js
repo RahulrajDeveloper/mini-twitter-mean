ef22e50745f238a2e4accb5f61e119274cf8bc8e
Rahulraj <rahulraj3798@gmail.com>
2023-01-31 18:24:11 +0530

    changes

--- db/db.js
+++ db/db.js
@@ -4,8 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser : true, useUnifiedTopology: true
+    // mongoose.connect(process.env.MONGO_URL, {
+    mongoose.connect('mongodb+srv://rahul:raj@mean.7zcxrx9.mongodb.net/NEWPROJECT', {
+        useNewUrlParser: true, useUnifiedTopology: true
     });
     console.log("Connected to database!!");
 }
